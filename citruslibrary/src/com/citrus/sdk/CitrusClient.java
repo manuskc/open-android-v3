746253fb42947d33ff0eb1a3c7361a3250fea415
Salil Godbole <salilgodbole@gmail.com>
2015-10-06 17:59:32 +0530

    init dp client

--- citruslibrary/src/com/citrus/sdk/CitrusClient.java
+++ citruslibrary/src/com/citrus/sdk/CitrusClient.java
@@ -171,6 +171,7 @@ public class CitrusClient {
             if (validate()) {
                 initRetrofitClient();
                 initCitrusBaseUrlClient();
+                initDynamicPricingClient();
             }
 
             // TODO: Remove full dependency on this class.
